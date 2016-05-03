c6574fe6ee9d247bde6a743f9c6499b1e5cff64e
macbook <592944119@qq.com>
2016-05-03 14:20:32 +0800

    添加测试

--- Empty/Empty/ViewController.m
+++ Empty/Empty/ViewController.m
@@ -18,6 +18,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     进行测试
+     */
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
